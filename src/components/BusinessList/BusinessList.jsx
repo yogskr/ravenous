db2e7878cc6011c2d027a4fcd81cc5fb6dd494c3
yogskr <yogakrisanta@gmail.com>
2024-04-15 21:38:00 +0700

    Rendered the businesses array using the .map() method and passed the businesses prop from the App component

--- src/components/BusinessList/BusinessList.jsx
+++ src/components/BusinessList/BusinessList.jsx
@@ -1,15 +1,13 @@
+/* eslint-disable react/prop-types */
 import Business from '../Business/Business';
-import styles from 'BusinessList.module.css';
+import styles from './BusinessList.module.css';
 
-export default function BusinessList() {
+export default function BusinessList({ businesses }) {
   return (
-    <div className={styles.businessList}>
-      <Business />
-      <Business />
-      <Business />
-      <Business />
-      <Business />
-      <Business />
+    <div className={styles.businessListContainer}>
+      {businesses.map((business) => (
+        <Business key={business.name} business={business} />
+      ))}
     </div>
   );
 }
