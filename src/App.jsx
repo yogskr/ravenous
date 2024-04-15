964de6549d77fdb6035bf2127f4b6ac3be1a55dd
yogskr <yogakrisanta@gmail.com>
2024-04-15 21:34:15 +0700

    Created an object and an array for dummy testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,28 @@
+import SearchBar from './components/SearchBar/SearchBar';
+import BusinessList from './components/BusinessList/BusinessList';
+import styles from './App.module.css';
+
+const business = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Favortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90,
+};
+
+const businesses = [business, business, business, business, business, business];
+
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <SearchBar />
+      <main className={styles.container}>
+        <BusinessList businesses={businesses} />
+      </main>
+    </>
+  );
 }
