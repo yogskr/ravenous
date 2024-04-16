8bfe01f43fee6c66c73de708d427e3950a3e3cf4
yogskr <yogakrisanta@gmail.com>
2024-04-16 15:26:27 +0700

    Created state management for the input fields and the sort by option

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,4 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
+import { useState } from 'react';
+
 import styles from './SearchBar.module.css';
 
 const sortByOptions = {
@@ -8,9 +10,40 @@ const sortByOptions = {
 };
 
 export default function SearchBar() {
+  // Manage sortByOptions
+  const [selectedSortBy, setSelectedSortBy] = useState('best_match');
+  const handleSortByOption = (option) => {
+    setSelectedSortBy(option);
+  };
+
+  // Manage businessInput
+  const [businessInput, setBusinessInput] = useState('');
+  const handleBusinessInput = (e) => {
+    setBusinessInput(e.target.value);
+  };
+
+  // Manage locationInput
+  const [locationInput, setLocationInput] = useState('');
+  const handleLocationInput = (e) => {
+    setLocationInput(e.target.value);
+  };
+
+  const handleClick = () => {
+    businessInput();
+    locationInput();
+    selectedSortBy();
+  };
+
   const renderSortByOptions = () =>
     Object.entries(sortByOptions).map(([text, value]) => (
-      <li key={value} className={styles.sortByOptionList}>
+      <li
+        key={value}
+        className={
+          selectedSortBy === value
+            ? `${styles.selectedSortByOptionList}`
+            : styles.sortByOptionList
+        }
+        onClick={() => handleSortByOption(value)}>
         {text}
       </li>
     ));
@@ -27,14 +60,20 @@ export default function SearchBar() {
             type="text"
             placeholder="Search Businesses"
             className={styles.searchInput}
+            value={businessInput}
+            onChange={handleBusinessInput}
           />
           <input
             type="text"
             placeholder="Where?"
             className={styles.searchInput}
+            value={locationInput}
+            onChange={handleLocationInput}
           />
         </div>
-        <button type="button">Let's Go</button>
+        <button type="button" onClick={handleClick}>
+          Let's Go
+        </button>
       </div>
     </header>
   );
